ac4966e37d6786e0439cf07ea08a299444df4cc7
Collin <collin@Zipcoders-MBP.hsd1.de.comcast.net>
2022-12-11 18:27:55 -0500

    basics working

--- index.js
+++ index.js
@@ -4,56 +4,39 @@ if (form != null) {
         var formData = new FormData(form);
         console.log(formData);
         var text = formData.get('defineword');
-        var apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/" + text;
-        fetch(apiUrl)
+        var apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/" + text; //URL from API of specific word where JSON objects are returned
+        fetch(apiUrl) //fetch JSON objects from the specific URL for the word inputted
             .then(function (response) {
-            return response.json();
+            return response.json(); //repsonse from fetch as JSON object
         })
             .then(function (data) {
-            var oldList = document.querySelector('.list-group');
-            var listParent = document.createElement('ul');
-            listParent.classList.add('list-group');
+
+            var oldList = document.querySelector('.list-group'); //returns first element that matches the specified selector and save it as oldList
+            var listParent = document.createElement('ul'); //creating element in TS for html
+            listParent.classList.add('list-group'); //returns a live DOMTokenList collection of the class attributes of the listParent.
+            
             var firstList = document.createElement('li');
             firstList.classList.add('list-group-item');
             firstList.textContent = data[0].word;
             listParent.append(firstList);
-            for (var i = 0; i < data.length; i++) {
-                for (var j = 0; j < data[i].meanings.length; j++) {
-                    var partOfSpeechItem = document.createElement('li');
-                    partOfSpeechItem.textContent = 'Parts of Speech: ' + data[i].meanings[j].partOfSpeech;
-                    var definitionTitle = document.createElement('li');
+
+            for (var i = 0; i < data.length; i++) { //iterate through each of the result sets for any given word by each data object
+                for (var j = 0; j < data[i].meanings.length; j++) { //iterating through results within each data object of that word
+                    
+                    var definitionTitle = document.createElement('li'); //defition title
                     definitionTitle.textContent = 'Definition:';
-                    var definitionItemList = document.createElement('ul');
+                    
+                    var definitionItemList = document.createElement('ul'); //definition as a list for the word selected
                     definitionTitle.append(definitionItemList);
-                    for (var k = 0; k < data[i].meanings[j].definitions.length; k++) {
+                    
+                    for (var k = 0; k < data[i].meanings[j].definitions.length; k++) { //getting meanings, definitions for each
                         var definitionItem = document.createElement('li');
                         definitionItem.textContent = data[i].meanings[j].definitions[k].definition;
                         definitionItemList.append(definitionItem);
                     }
-                    listParent.append(partOfSpeechItem, definitionTitle);
+                    listParent.append(definitionTitle);
                 }
             }
-            // const loopList: HTMLLIElement = document.createElement('li');
-            // loopList.classList.add('list-group-item');
-            // for(let i = 0; i < data.length; i++) {
-            //   if(data[i] != null) {
-            //     loopList.textContent = 'Word:' + data[i].word;
-            //     listParent.append(loopList);
-            //   }
-            //     for(let j = 0; j < data[i].meanings.length; j++) {
-            //       if(data[i].meanings[j] != null) {
-            //         loopList.textContent = 'Meanings:' + data[i].meanings[j].meanings;
-            //         listParent.append(loopList);
-            //       }
-            //         for(let k = 0; k < data[i].meanings[j].definitions.length; k++) {
-            //           if(data[i].meanings[j].definitions[k] != null) {
-            //             loopList.textContent = 'Definitions:' + data[i].meanings[j].definitions[k].definition;
-            //             listParent.append(loopList);
-            //       }
-            //     }
-            //   }
-            // }
-            // listParent.append(firstList, secondList);
             oldList === null || oldList === void 0 ? void 0 : oldList.replaceWith(listParent);
             console.log(data);
         })["catch"](function (error) { return console.log(error); });
@@ -62,12 +45,3 @@ if (form != null) {
     };
 }
 ;
-// 
-// fetch(apiUrl, {
-//   method: 'GET',
-//   headers: {
-//     Accept: 'application.json',
-//     'Content-Type': 'application/json'
-//   },
-// })
-// .then((response) => console.log(response.text()));
--- index.ts
+++ index.ts
@@ -1,70 +1,45 @@
 const form: HTMLFormElement | null = document.querySelector('#defineform');
 
-if(form != null) {
+if(form != null) { 
   form.onsubmit = () => {
     const formData = new FormData(form);
 
     console.log(formData);
     const text = formData.get('defineword') as string;
-    const apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/" + text;
-    fetch(apiUrl)
+    const apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/" + text; //URL from API of specific word where JSON objects are returned
+    
+    fetch(apiUrl) //fetch JSON objects from the specific URL for the word inputted
     .then((response) => { 
-      return response.json()
+      return response.json() //repsonse from fetch as JSON object
     })
     .then((data) => {
-      const oldList = document.querySelector('.list-group');
+      const oldList = document.querySelector('.list-group'); //returns first element that matches the specified selector and save it as oldList
 
-      const listParent: HTMLUListElement = document.createElement('ul');
-      listParent.classList.add('list-group');
+      const listParent: HTMLUListElement = document.createElement('ul'); //creating element in TS for html
+      listParent.classList.add('list-group'); //returns a live DOMTokenList collection of the class attributes of the listParent.
 
       const firstList: HTMLLIElement = document.createElement('li');
       firstList.classList.add('list-group-item');
       firstList.textContent = data[0].word;
       listParent.append(firstList);
 
-      for (let i = 0; i < data.length; i++) {
-        for (let j = 0; j < data[i].meanings.length; j++) {
-            const partOfSpeechItem: HTMLLIElement = document.createElement('li');
-            partOfSpeechItem.textContent = 'Parts of Speech: ' + data[i].meanings[j].partOfSpeech;
+      for (let i = 0; i < data.length; i++) { //iterate through each of the result sets for any given word by each data object
+        for (let j = 0; j < data[i].meanings.length; j++) { //iterating through results within each data object of that word
 
-            const definitionTitle: HTMLLIElement = document.createElement('li');
+            const definitionTitle: HTMLLIElement = document.createElement('li'); //display "Definition:""
             definitionTitle.textContent = 'Definition:';
 
-            const definitionItemList: HTMLUListElement = document.createElement('ul');
+            const definitionItemList: HTMLUListElement = document.createElement('ul'); //definition as a list for the word selected
             definitionTitle.append(definitionItemList);
 
-            for (let k = 0; k < data[i].meanings[j].definitions.length; k++) {
+            for (let k = 0; k < data[i].meanings[j].definitions.length; k++) { //getting meanings, definitions for each
                 const definitionItem: HTMLLIElement = document.createElement('li');
                 definitionItem.textContent = data[i].meanings[j].definitions[k].definition;
                 definitionItemList.append(definitionItem);
             }
-            listParent.append(partOfSpeechItem, definitionTitle);
+            listParent.append(definitionTitle);
         }
     }
-      // const loopList: HTMLLIElement = document.createElement('li');
-      // loopList.classList.add('list-group-item');
-      
-      // for(let i = 0; i < data.length; i++) {
-      //   if(data[i] != null) {
-      //     loopList.textContent = 'Word:' + data[i].word;
-      //     listParent.append(loopList);
-      //   }
-      //     for(let j = 0; j < data[i].meanings.length; j++) {
-      //       if(data[i].meanings[j] != null) {
-      //         loopList.textContent = 'Meanings:' + data[i].meanings[j].meanings;
-      //         listParent.append(loopList);
-      //       }
-      //         for(let k = 0; k < data[i].meanings[j].definitions.length; k++) {
-      //           if(data[i].meanings[j].definitions[k] != null) {
-      //             loopList.textContent = 'Definitions:' + data[i].meanings[j].definitions[k].definition;
-      //             listParent.append(loopList);
-      //       }
-      //     }
-      //   }
-      // }
-
-
-      // listParent.append(firstList, secondList);
 
       oldList?.replaceWith(listParent);
       console.log(data);
@@ -75,13 +50,3 @@ if(form != null) {
     return false; // prevent reload
   };
 };
-// 
-  
-  // fetch(apiUrl, {
-  //   method: 'GET',
-  //   headers: {
-  //     Accept: 'application.json',
-  //     'Content-Type': 'application/json'
-  //   },
-  // })
-  // .then((response) => console.log(response.text()));
